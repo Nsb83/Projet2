b8cea2cf311a7c9ebcc58effd1ef8590f9fbfe19
JN Bourrat <jn.brrt@yahoo.fr>
2018-10-03 18:05:59 +0200

    Created router links and result-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ResultsPageComponent } from './results-page/results-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'results', component: ResultsPageComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchBarComponent,
+    ResultsPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
