cef4cbe4f787dd90a396e71503fd4ca81799aea6
JN Bourrat <jn.brrt@yahoo.fr>
2018-10-11 17:01:17 +0200

    Removed home button

--- src/app/search-by-artist.service.ts
+++ src/app/search-by-artist.service.ts
@@ -15,7 +15,6 @@ export class SearchByArtistService {
 
   getResults(userInput): Observable<any> {
     this._url = `https://api.songkick.com/api/3.0/search/artists.json?apikey=R82Hox7PJZDJyV0G&query=${userInput}`;
-    console.log(`Le service search-by-artiste a utilisé l'url suivante : ${this._url}`);
     return this.http.get<any>(this._url);
   }
 }
