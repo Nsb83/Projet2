f17096a9b45f0bc7bfd15621745691aa5636636a
JN Bourrat <jn.brrt@yahoo.fr>
2018-10-16 15:53:39 +0200

    Deleted observables in service

--- src/app/search-by-artist.service.ts
+++ src/app/search-by-artist.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchByArtistService {
 
-  public artistDisplayName:string 
+  public artistDisplayName:string
   private _url: string;
   private _artistUrl: string;
   private _imgDescr: string;
@@ -16,17 +15,17 @@ export class SearchByArtistService {
     private http: HttpClient,
   ) { }
 
-  getArtistConcerts(artistId): Observable<any> {
+  getArtistConcerts(artistId) {
     this._artistUrl = `https://api.songkick.com/api/3.0/artists/${artistId}/calendar.json?apikey=R82Hox7PJZDJyV0G`;
     return this.http.get<any>(this._artistUrl);
   }
 
-  getResults(userInput): Observable<any> {
+  getResults(userInput) {
     this._url = `https://api.songkick.com/api/3.0/search/artists.json?apikey=R82Hox7PJZDJyV0G&query=${userInput}`;
     return this.http.get<any>(this._url);
   }
 
-  getImgDescr(artistDisplayName): Observable<any> {
+  getImgDescr(artistDisplayName) {
     this._imgDescr = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistDisplayName}&api_key=9b7579d5f409106928353935ac0ab5ab&format=json`
     console.log(this._imgDescr)
     return this.http.get<any>(this._imgDescr);
