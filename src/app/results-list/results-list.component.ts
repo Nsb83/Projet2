ebd3ae7b73cf5ea42a0d976c2cec5ebf3192495c
Mathieu Kanel <mathieu.kanel@gmail.com>
2018-11-07 15:24:47 +0100

    active filter button color

--- src/app/results-list/results-list.component.ts
+++ src/app/results-list/results-list.component.ts
@@ -34,6 +34,11 @@ export class ResultsListComponent implements OnInit {
   public filterVenues: boolean = true;
   public filterCities: boolean = true;
   
+  public activeButtonArtists: boolean = false;
+  public activeButtonVenues: boolean = false;
+  public activeButtonCities: boolean = false;
+  public activeButtonAll: boolean = false;
+  
   @HostListener('window:resize')
   onResize() {this.currentWindowWidth = window.innerWidth}
 
@@ -65,24 +70,40 @@ export class ResultsListComponent implements OnInit {
     this.filterArtists = true;
     this.filterVenues = false;
     this.filterCities = false;
+    this.activeButtonArtists = true;
+    this.activeButtonVenues = false;
+    this.activeButtonCities = false;
+    this.activeButtonAll = false;
   }
 
   showVenues() {
     this.filterVenues = true;
     this.filterArtists = false;
     this.filterCities = false;
+    this.activeButtonArtists = false;
+    this.activeButtonVenues = true;
+    this.activeButtonCities = false;
+    this.activeButtonAll = false;
   }
 
   showAll() {
     this.filterArtists = true;
     this.filterVenues = true;
     this.filterCities = true;
+    this.activeButtonArtists = false;
+    this.activeButtonVenues = false;
+    this.activeButtonCities = false;
+    this.activeButtonAll = true;
   }
 
   showCities() {
     this.filterArtists = false;
     this.filterVenues = false;
     this.filterCities = true;
+    this.activeButtonArtists = false;
+    this.activeButtonVenues = false;
+    this.activeButtonCities = true;
+    this.activeButtonAll = false;
   }
 
 
