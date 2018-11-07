e2999e8d6e1d0cf00a30667ea4d39487d1166445
JN Bourrat <jn.brrt@yahoo.fr>
2018-11-07 14:00:31 +0100

    Formatted date, almost good

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbDateAdapter, NgbDateStruct, NgbDateNativeAdapter} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-date-picker',
   templateUrl: './date-picker.component.html',
-  styleUrls: ['./date-picker.component.css']
+  styleUrls: ['./date-picker.component.css'],
+  providers: [{provide: NgbDateAdapter, useClass: NgbDateNativeAdapter}]
 })
 export class DatePickerComponent implements OnInit {
 
-
-  date;
-  date2;
-
   constructor() { }
 
+  get today() {
+    return new Date();
+  }
 
   ngOnInit() {
-    // this.date2 = this.date.year + '-' + this.date.month + '-' + this.date.day;
+
   }
 
 }
+
+
