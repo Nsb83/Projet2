feb75c07600c1e14aaf1962f778e3b5c03bdbf61
JN Bourrat <jn.brrt@yahoo.fr>
2018-11-08 02:21:18 +0100

    Implemented minDate on date filters in order to not send a wrong request to the api

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -13,8 +13,29 @@ export class DatePickerComponent implements OnInit {
 
   @Output() datesMinMax = new EventEmitter<String[]>();
 
+  private dateMin: any = this.today;
+  private dateMax: any = this.today;
+
+  get dateMinObj() {
+    if (this.dateMin.hasOwnProperty('year')) {
+      return this.dateMin;
+    } else {
+      const dateMinObj = this.convertDateToObj(this.dateMin);
+      return dateMinObj;
+    }
+  }
+
   get today() {
-    return new Date();
+    return this.convertDateToObj(new Date());
+  }
+
+  convertDateToObj(date) {
+    const dateObj = {
+      year: date.getFullYear(),
+      month: date.getMonth() + 1,
+      day: date.getDate()
+    };
+    return dateObj;
   }
 
   convertDate(date) {
@@ -33,6 +54,8 @@ export class DatePickerComponent implements OnInit {
 
   onClearingFilters() {
     this.datesMinMax.emit(['', '']);
+    this.dateMin = this.today;
+    this.dateMax = this.today;
   }
 
   ngOnInit() {
